c4a41df91a7d9d5918fe504048e4a5bd9144f395
ashraf7ussein <ashrafhussein1997@gmail.com>
2025-05-23 17:40:40 +0300

    fetch data using react query

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { Grid, Text } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 
 interface Card {
   id: string;
@@ -12,29 +12,28 @@ interface Card {
 }
 
 const CardsGrid = () => {
-  const [cards, setCards] = useState<Card[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const randomPage = Math.floor(Math.random() * 100);
-
+  const randomPage = Math.floor(Math.random() * 100);
+  const fetchCards = () =>
     apiClient
       .get(`/cards?pageSize=5&page=${randomPage}`)
-      .then((res) => setCards(res.data.data))
-      .catch((err) => setError(err.message));
-  }, []);
+      .then((res) => res.data.data);
+
+  const { data: cards, error } = useQuery<Card[], Error>({
+    queryKey: ["cards"],
+    queryFn: fetchCards,
+  });
 
   return (
     <>
       {error ? (
-        <p className="mt-35 text-red-500">{error}</p>
+        <p className="mt-35 text-red-500">{error.message}</p>
       ) : (
         <>
           <Text mt="9" size="5">
             Featured Cards
           </Text>
           <Grid gap="3" mt="3" columns={{ initial: "2", md: "3", lg: "5" }}>
-            {cards.map((card) => (
+            {cards?.map((card) => (
               <div key={card.id}>
                 <img
                   className="rounded-lg shadow hover:shadow-lg transition-transform hover:scale-105 duration-300"
